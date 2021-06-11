2724f920657194a4740f1f8faecafc0f83694fa8
CHINTU <harrypetersun@outlook.com>
2021-06-12 02:06:14 +0530

    Fix time

--- ARCH/chroot.sh
+++ ARCH/chroot.sh
@@ -13,8 +13,6 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Set date time
-timedatectl set-timezone Asia/Kolkata
-timedatectl set-ntp true
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 
