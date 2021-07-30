a865cf35dbfb18034b1e08ea8c16612c92e1edbb
CHINTU <harrypetersun@outlook.com>
2021-07-30 10:54:07 +0000

    USE NEW STUFF

--- ARCH/chroot.sh
+++ ARCH/chroot.sh
@@ -1,72 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#Install software
-pacman -S networkmanager \
-    intel-ucode ufw sudo \
-    xfce4 lightdm-gtk-greeter \
-    xorg-xinit xorg-server grub \
-    efibootmgr --noconfirm --needed
+#TIME
+ln -sf /usr/share/zoneinfo/"$RNAME" /etc/localtime && hwclock --systohc
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen && locale-gen && echo 'LANG=en_US.UTF-8' >/etc/locale.conf
 
-# Set locale to en_US.UTF-8 UTF-8
-sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >>/etc/locale.conf
+# HOSTNAME
+echo "$HNAME" >/etc/hostname
+echo -e "127.0.0.1 $HNAME\n::1 $HNAME\n127.0.1.1 $HNAME.localdomain $HNAME" >>/etc/hosts
 
-# Set date time
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-hwclock --systohc
+# ROOT-PASSWORD
+br && echo "Setting new Root password [Keep powerfull password in mind]"
+passwd
 
-# Set hostname
-echo "3542" >>/etc/hostname
-echo "127.0.0.1	localhost
-::1		localhost
-127.0.1.1 3542.localdomain 3542" >>/etc/hosts
+# NEW-USER
+br && echo "Setting new user $USER1 [Keep powerfull password in mind]"
+groupadd sudo && useradd -mG wheel,sudo -s /usr/bin/bash "$USER1"
+passwd "$USER1"
+sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+ALL\)/\1/' /etc/sudoers
 
-# Generate initramfs
-mkinitcpio -P
+# ENABLE SYSTEM SERVICE
+systemctl enable NetworkManager.service
+systemctl enable ufw.service
 
-# Install grub with efi
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
-grub-mkconfig -o /boot/grub/grub.cfg
+# GRUB CONFIG
+sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"i8042.nopnp\"/g" /etc/default/grub
 
-# Firewall setup
+# INSTALL PARU
+git clone --depth=1 https://aur.archlinux.org/paru-bin.git paru
+cd paru && sudo -u "$USER1" makepkg -si && rm -rf /paru
+
+# PACMAN CONFIG
+# Add chromium pacman config
+curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/x86_64/home_ungoogled_chromium_Arch.key' | pacman-key -a -
+echo -e '\n[home_ungoogled_chromium_Arch]\nSigLevel = Required TrustAll' | tee -a /etc/pacman.conf
+echo -e 'Server = https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/$arch' | tee -a /etc/pacman.conf
+# Enabling multilib in pacman
+sed -i '93s/#\[/\[/' /etc/pacman.conf && sed -i '94s/#I/I/' /etc/pacman.conf
+# Tweaking pacman, uncomment options Color, TotalDownload and VerbosePkgList
+sed -i '34s/#C/C/' /etc/pacman.conf && sed -i '35s/#T/T/' /etc/pacman.conf && sed -i '37s/#V/V/' /etc/pacman.conf
+
+# FIREWALL
 ufw default deny incoming
 ufw default allow outgoing
 ufw enable
 
-#install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay/ || exit
-makepkg -si PKGBUILD
-cd .. && rm -rf ./yay
-
-# Enable service
-systemctl enable lightdm.service
-systemctl enable NetworkManager.service
-systemctl enable ufw.service
-
-# Set root password
-clear
-echo "Set root password"
-passwd
-
-# add new user
-clear
-groupadd sudo
-echo "Adding new user"
-read -re -i "chintu" USER1
-useradd -m -G wheel,power,input,storage,uucp,network,sudo -s /usr/bin/bash "$USER1"
-sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
-echo "Set password for new user $USER1"
-passwd "$USER1"
-
-#Add profile.d scripts
-clear
-echo "Adding profile.d scripts"
+# PROFILE.D [WIP]
 rm -rf /etc/profile.d/*alias* /etc/profile.d/*ps1*
 curl -LO https://raw.githubusercontent.com/Jrchintu/CDN/main/ARCH/req/alias.sh >>/etc/profile.d/alias.sh
 curl -LO https://raw.githubusercontent.com/Jrchintu/CDN/main/ARCH/req/ps1.sh >>/etc/profile.d/ps1.sh
 chmod a+x /etc/profile.d/*
-
-#EXIT
-echo "Configuration done. exiting chroot or add extra stuff of ur choice"
