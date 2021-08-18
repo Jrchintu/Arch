f2347db728a86d1b96d5e0e66a40b4d2a7f96a4d
CHINTU <harrypetersun@outlook.com>
2021-08-19 01:40:40 +0530

    Update env.sh

--- AOSP/env.sh
+++ AOSP/env.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 LC_ALL=C && LANG=C
 
+# EXPORT
+export SUSER="${SUDO_USER:-${USER}}"
+noroot() {
+  sudo -H -u "$SUSER" bash -c "$1"
+}
+
 # ENV SETUP
-sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove --purge -y
+sudo apt update -y && sudo apt upgrade -y
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt install git-core p7zip-full aria2 rclone pigz -y
+apt remove google-cloud-sdk --purge -y
+sudo apt autoremove --purge -y
+
 curl https://storage.googleapis.com/git-repo-downloads/repo >./repo
 sudo mv repo /usr/bin/repo && sudo chmod a+x /usr/bin/repo
-PATH=/usr/bin/repo:$PATH
+PATH="/usr/bin/repo:$PATH"
 git clone --depth=1 https://github.com/akhilnarang/scripts.git scripts
 cd scripts && sudo bash setup/android_build_env.sh && cd .. && rm -rf scripts
 
 # BASHRC
-cat <<EOF >>"$HOME"/.bashrc
+cat <<EOF >>/home/"$SUSER"/.bashrc
 export USE_CCACHE=1
 export CCACHE_DIR="$HOME/.ccache"
 export CCACHE_EXEC="$(which ccache)"
@@ -30,32 +39,26 @@ EOF
 ccache -M 30G # OPTIONAL
 
 # ALIAS
-echo '' >>"$HOME"/.bashrc && echo '# ALias' >>"$HOME"/.bashrc
-echo 'alias fsync="repo sync  --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)"' >>"$HOME"/.bashrc
+echo '' >>/home/"$SUSER"/.bashrc && echo '# ALias' >>/home/"$SUSER"/.bashrc
+echo 'alias fsync="repo sync  --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)"' >>/home/"$SUSER"/.bashrc
 
 # Functions
-echo '' >>"$HOME"/.bashrc && echo '# Functions' >>"$HOME"/.bashrc
+echo '' >>/home/"$SUSER"/.bashrc && echo '# Functions' >>/home/"$SUSER"/.bashrc
 echo 'mkcd(){
     mkdir $1 && cd $1
-}' >>"$HOME"/.bashrc
+}' >>/home/"$SUSER"/.bashrc
 echo 'br() {
     for ((i = 1; i <= $(tput cols); i++)); do echo -n -; done
-}' >>"$HOME"/.bashrc
+}' >>/home/"$SUSER"/.bashrc
 
 # Source bashrc
-source "$HOME"/.bashrc
+source /home/"$SUSER"/.bashrc
 
 # MKDIR
 mkdir rom && chmod -R 777 rom
 cd rom && mkdir .repo && br
 read -r -p "Do you want to clone local_manifest? (y/n) " lmanif
 if [ "${lmanif}" = "y" ]; then
-    read -rep "What Branch From Local_manifest Repo U wanna clone [Case Sensitive]? " LMBRANCH
-    git clone https://github.com/Jrchintu/local_manifest --depth 1 -b "$LMBRANCH" .repo/local_manifests
+  read -rep "What Branch From Local_manifest Repo U wanna clone [Case Sensitive]? " LMBRANCH
+  git clone https://github.com/Jrchintu/local_manifest --depth 1 -b "$LMBRANCH" .repo/local_manifests
 fi
-
-# GIT
-git config --global user.email "chintu@gcp.com"
-git config --global user.name "Chintu"
-git config --global color.ui true
-git config --global core.editor nano
