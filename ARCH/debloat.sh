939ec00a34433fc2de0fc5e1c2adcc0db224a11b
Jrchintu <Harrypetersun@outlook.com>
2021-09-14 23:26:04 +0530

    Add debloat script
    Signed-off-by: Jrchintu <Harrypetersun@outlook.com>

---
+++ ARCH/debloat.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+# REMOVE Plsma junk
+pacman -Rcns kate kate5-data kate-data kwrite kwrited kmail akonadi-server juk dragonplayer k3b k3b-data k3b-i18n konqueror akregator oxygen plasma-vault \
+	plasma-workspace-wallpapers kdeplasma-addons plasma-thunderbolt network-manager-sstp
+
+# Remove Xfce junk
+pacman -Rcns xfce4-terminal xfce4-appfinder tumbler
+
+# OPTIONAL DEPENDENCIES
+pacman -Qtqtdq | pacman -Rcns -s
+pacman -R $(pacman -Qdtq)
+
+# Add/Remove Some Junk
+pacman -Rcns xfce4-appfinder xterm rsh-server telnet-server
+paru -Scc
+rm -rfv ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
+
+# Junk Themes
+JUNKTHEME=(
+Adept
+Agua
+Agualemon
+Alternate
+Atlanta
+Atlanta2
+B5
+B6
+Basix
+BBS
+Beastie
+Biz
+Blackwall
+Bright
+Buzz
+Clearlooks
+Coldsteel
+Coolclean
+Crux
+Cruxish
+Curve
+Daloa
+Default
+Default-4.0
+Default-4.2
+Default-4.4
+Default-4.6
+Default-4.8
+Default-hdpi
+Default-xhdpi
+Defcon-IV
+Eazel-blue
+Elberg
+Emacs
+Exocet
+Fbx
+G2
+Galaxy
+Gaudy
+Gelly
+Gnububble
+Gorilla
+Gtk
+Iceg
+Industrial
+Kde
+Kde1
+Keramik
+Kindaker
+Kleanux
+Kokodi
+Koynacity
+Linea
+LineArt
+Meenee
+Metabox
+Microcurve
+Microdeck
+Microdeck2
+Microdeck3
+Microgui
+Mist
+Mofit
+Moheli
+Next
+Ops
+Opta
+Oroborus
+Perl
+Pills
+Piranha
+Platinum
+Prune
+Quiet-purple
+Quinx
+R9X
+Raleigh
+Redmond
+RedmondXP
+Retro
+Sassandra
+Silverado
+Slick
+Slimline
+Smallscreen
+Smoke
+Smoothwall
+Stoneage
+Symphony
+Synthetic
+Tabs
+Tgc
+Tgc-large
+Therapy
+ThinIce
+Today
+Totem
+Trench
+Triviality
+Tubular
+TUX
+Tyrex
+Variation
+Wallis
+Waza
+Wildbush
+Xfce
+'ZOMG-PONIES!'
+)
+
+# Debloat themes now
+for A in "${JUNKTHEME[@]}"; do
+    rm -rvf "/usr/share/themes/$A"
+done
